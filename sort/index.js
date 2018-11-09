873082cef081b980267e498b7f7ab0a49b52a893
Jakkarohith <35620857+Jakkarohith@users.noreply.github.com>
2018-11-09 14:34:02 +0530

    Update index.js

--- sort/index.js
+++ sort/index.js
@@ -4,9 +4,9 @@ var arr=new Array();
 console.log("Enter the array elements");
 for(let i=0;i<n;i++)
 {
-    let temp=parseInt(sb.question());
-    arr[i]=temp;
+    arr[i]=parseInt(sb.question());;
 }
+
 console.log(arr); 
 for (let i=0; i<n-1;i++) 
     for (let j=0;j<n-i-1;j++) 
